c7cf39e6ff3bc9c3aaa1f5434a270496217186b0
abhinavhinger12 <abhinavhinger12@gmail.com>
2019-03-17 21:18:23 +0530

    Message Creation

--- client.c
+++ client.c
@@ -47,6 +47,54 @@ int str_to_pnum(char str[]){
 	return n;
 }
 
+void crc(char *input,char *key,char *result,int keylen,int msglen){
+	char key1[30],temp[30],quot[100],rem[30];
+	int i,j;
+
+	strcpy(key1,key);
+	for(i=0;i<keylen-1;i++)
+	{
+		input[msglen+i]='0';
+	}
+	for(i=0;i<keylen;i++)
+		temp[i]=input[i];
+	for(i=0;i<msglen;i++)
+	{
+		quot[i]=temp[0];
+		if(quot[i]=='0')
+			for(j=0;j<keylen;j++)
+				key[j]='0';
+			else{
+				for(j=0;j<keylen;j++)
+					key[j]=key1[j];
+			}
+			for(j=keylen-1;j>0;j--)
+			{
+				if(temp[j]==key[j])
+					rem[j-1]='0';
+				else
+					rem[j-1]='1';
+			}
+			rem[keylen-1]=input[i+keylen];
+			strcpy(temp,rem);
+		}
+		strcpy(rem,temp);
+		printf("\nQuotient is ");
+		for(i=0;i<msglen;i++)
+			printf("%c",quot[i]);
+		printf("\nRemainder is ");
+		for(i=0;i<keylen-1;i++)
+			printf("%c",rem[i]);
+		printf("\nFinal data is: ");
+		for(i=0;i<msglen;i++){
+			result[i]=input[i];
+		}
+		for(i=0;i<keylen-1;i++){
+			result[msglen+i] = rem[i];
+		}
+	}
+
+
 
 int main(int argc,char *argv[]) 
 { 
@@ -65,8 +113,25 @@ int main(int argc,char *argv[])
 	struct sockaddr_in client_address, server_address; 
     //Initializing Socket Addresses for client and server structure has 4 attrs:- family, port, address, zero(for padding the data)
 
+	
 	char *message = "Hello from client"; 
     //The message to be sent
+	int i,j,keylen,msglen;
+	char input[100], key[30],temp[30],quot[100],rem[30],key1[30];
+	printf("Enter Data: ");
+	gets(input);
+	printf("Enter Key: ");
+	gets(key);
+	keylen=strlen(key);
+	msglen=strlen(input);
+
+	char result[keylen+msglen-1];
+	crc(input,key,result,keylen,msglen);
+
+	for(i=0;i<msglen+keylen-1;i++){
+		printf("%c",result[i]);			
+	}
+	
 
 	int sock = 0;
 
@@ -97,7 +162,7 @@ int main(int argc,char *argv[])
     // coverting sockaddr_in(socket for IP based communication) to sockaddr(generic socket)
     // Check if connection can be initiated, if yes returns 0, else -1 
 
-	send(sock,message,strlen(message),0); 
+	send(sock,result,strlen(result),0); 
 
     //send the message, the 0 as last parameter is the field for flags.
 	printf("Message Sent Successfully\n"); 
