34a5a1f2faf2f466cad017a096c78d9a24c9d6d7
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2019-03-20 21:34:34 +0530

    Multiple clients running through pthreads

--- client.c
+++ client.c
@@ -10,7 +10,6 @@
 
 #define OUTPUT_BUFFER_SIZE 1024
 
-char subs[100];
 
 bool isNumber(char str[]){
 	int i=0;
@@ -47,110 +46,26 @@ int str_to_pnum(char str[]){
 	return n;
 }
 
-void crc(char *input,char *key,char *result,int keylen,int msglen){
-	char key1[30],temp[30],quot[100],rem[30];
-	int i,j;
 
-	strcpy(key1,key);
-	for(i=0;i<keylen-1;i++)
-	{
-		input[msglen+i]='0';
-	}
-	for(i=0;i<keylen;i++)
-		temp[i]=input[i];
-	for(i=0;i<msglen;i++)
-	{
-		quot[i]=temp[0];
-		if(quot[i]=='0')
-			for(j=0;j<keylen;j++)
-				key[j]='0';
-			else{
-				for(j=0;j<keylen;j++)
-					key[j]=key1[j];
-			}
-			for(j=keylen-1;j>0;j--)
-			{
-				if(temp[j]==key[j])
-					rem[j-1]='0';
-				else
-					rem[j-1]='1';
-			}
-			rem[keylen-1]=input[i+keylen];
-			strcpy(temp,rem);
-		}
-		strcpy(rem,temp);
-		printf("\nQuotient is ");
-		for(i=0;i<msglen;i++)
-			printf("%c",quot[i]);
-		printf("\nRemainder is ");
-		for(i=0;i<keylen-1;i++)
-			printf("%c",rem[i]);
-		printf("\nFinal data is: ");
-		for(i=0;i<msglen;i++){
-			result[i]=input[i];
-		}
-		for(i=0;i<keylen-1;i++){
-			result[msglen+i] = rem[i];
-		}
-	}
-/*
-BER -> Bit Error Rate = No of bits to have error in a bit transmitted on an average
-So we randomly choose these bits and generate error
-*/
-void randomError(float ber,char *pure,char *noisy,int size){
-	int n = floor(ber*size);
-
-	int indx[n];
-	for(int i=0;i<n;i++){
-		indx[i] = rand() % size-1;
-		printf("%d\n",indx[i]);
-	}
-	printf("\n");
-	for(int i=0;i<n;i++){
-		if(pure[indx[i]]) noisy[indx[i]] = '0';
-		else noisy[indx[i]] = '1';
-	}
-}
-
-int main(int argc,char *argv[]) 
-{ 
+int main(int argc,char *argv[]){ 
 	if(argc != 3){
 		perror("Missing/Extra Arguments provided.\n");
-		perror("Usage:- ./a.out <Server IP Address> <Server Port>\n");
+		perror("Usage:- ./<output_file> <Server IP Address> <Server Port>\n");
 		return -1;
 	}
 	//checking for whether correct number of arguments are provided
 	int port = str_to_pnum(argv[2]);
 	if(port==-1){
-		perror("Invalid Port Number Must be in range [0-65535]");
+		perror("Invalid Port Number Must be in range [0-65535]\n");
+		return -1;
 	}
 	//checking for port number
 
 	struct sockaddr_in client_address, server_address; 
-    //Initializing Socket Addresses for client and server structure has 4 attrs:- family, port, address, zero(for padding the data)
+	//Initializing Socket Addresses for client and server structure has 4 attrs:- family, port, address, zero(for padding the data)
 
-	
 	char *message = "Hello from client"; 
-    //The message to be sent
-	int i,j,keylen,msglen;
-	char input[100],temp[30],quot[100],rem[30],key1[30];
-	printf("Enter Data: ");
-	gets(input);
-
-	//CRC-8 Generator Polynomial
-	char key[10] = {'1','0','0','0','0','0','1','1','1'};
-	key[9] = '\0';
-	
-	keylen=strlen(key);
-	msglen=strlen(input);
-
-	char result[keylen+msglen-1];
-	crc(input,key,result,keylen,msglen);
-
-	for(i=0;i<msglen+keylen-1;i++){
-		printf("%c",result[i]);			
-	}
-	
+	//The message to be sent
 
 	int sock = 0;
 
@@ -158,36 +73,36 @@ int main(int argc,char *argv[])
 		perror("Socket creation error \n"); 
 		return -1; 
 	}
-    //Initializing the socket, The socket function creates an unbound socket and returns its file descriptor. SOCK_STREAM provides sequenced and reliable data transfer
+	//Initializing the socket, The socket function creates an unbound socket and returns its file descriptor. SOCK_STREAM provides sequenced and reliable data transfer
 
 	memset(&server_address,'0',sizeof(server_address)); 
-   	// filling server_address struct to all 0's
+	// filling server_address struct to all 0's
 
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(port); 
-    // htons:- host to network short, converts the given port number to hexa decimal depending on what machine type is it, big endian or little endian
+	// htons:- host to network short, converts the given port number to hexa decimal depending on what machine type is it, big endian or little endian
 
 
 	if(inet_pton(AF_INET,argv[1],&server_address.sin_addr) <= 0){ 
 		perror("Invalid server address / Address not supported \n"); 
 		return -1; 
 	} 
-    // Check if the given IP address belongs to the given family and convert it to binary and store it in server_address
+	// Check if the given IP address belongs to the given family and convert it to binary and store it in server_address
 
 	if (connect(sock,(struct sockaddr *)&server_address,sizeof(server_address)) < 0){ 
 		perror("Connection Failed \n"); 
 		return -1; 
 	}
-    // coverting sockaddr_in(socket for IP based communication) to sockaddr(generic socket)
-    // Check if connection can be initiated, if yes returns 0, else -1 
+	// coverting sockaddr_in(socket for IP based communication) to sockaddr(generic socket)
+	// Check if connection can be initiated, if yes returns 0, else -1 
 
-	send(sock,result,msglen+keylen-1,0); 
+	send(sock,message,strlen(message),0); 
 
-    //send the message, the 0 as last parameter is the field for flags.
+	//send the message, the 0 as last parameter is the field for flags.
 	printf("Message Sent Successfully\n"); 
 
 	char buffer[OUTPUT_BUFFER_SIZE] = {0};
-    //initalizing output buffer
+	//initalizing output buffer
 
 	int valread = read(sock,buffer,OUTPUT_BUFFER_SIZE); 
 	printf("%s\n",buffer); 
@@ -196,6 +111,6 @@ int main(int argc,char *argv[])
 	// printf("Message Sent Successfully\n");
 	// valread = read(sock,buffer,OUTPUT_BUFFER_SIZE); 
 	// printf("%s\n",buffer); 
-	// while(1){}
+	while(1){}
 	return 0; 
 } 
--- server.c
+++ server.c
@@ -1,34 +1,165 @@
 #include <stdio.h> 
-#include <string.h> //strlen 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <unistd.h> //close 
-#include <arpa/inet.h> //close 
-#include <sys/types.h> 
 #include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
-	
-#define TRUE 1 
-#define FALSE 0 
-#define PORT 8888 
-	
-int main(int argc , char *argv[]){
+#include <sys/types.h> 
+#include <stdlib.h> 
+// #include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <string.h> 
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <pthread.h>
 
-	char buffer[OUTPUT_BUFFER_SIZE] = {0}; 
-	int valread = read(client_socket,buffer,1024); 
-	printf("%s\n",buffer );
+#define OUTPUT_BUFFER_SIZE 1024
+#define MAX_PENDING_CONN 50
+#define RTT 0.5
 
-	char key[10] = {'1','0','0','0','0','0','1','1','1'};
-	key[9] = '\0';
+const char * IP_ADDRESS = "0.0.0.0";
+char client_message[2000];
+char buffer[OUTPUT_BUFFER_SIZE];
+int valread;
 
-	bool err = isErrorFree(buffer,key,strlen(buffer),strlen(key));
-	printf("%d\n",err);
+bool isNumber(char str[]){
+    int i=0;
+    if(str[0]=='-'){
+        i=1;
+    }
+    for(;str[i]!='\0';i++){
+        if(!isdigit(str[i])) return false;
+    }
+    return true;
+}
 
+int str_to_pnum(char str[]){
+    if(!isNumber(str)){
+        return -1;
+    }
+    int sign, i=0;
+    if(str[0]=='-'){  // Handle negative integers
+        sign = -1;
+    }
+    if(sign==-1){  // Set starting position to convert
+        i = 1;
+    }
+    int n = 0;
+    for(; str[i]!='\0'; i++) {
+        n = n * 10 + str[i] - '0';
+    }
+    if(sign==-1){
+        return -1;
+    }
+    if(n>65535){
+        return -1;
+    }
+    return n;
+}
 
-	char *message = "Hello from server"; 
-	send(client_socket , message , strlen(message) , 0 ); 
-	printf("Hello message sent\n"); 
+void printnew(int socketfd){
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    getpeername(socketfd,(struct sockaddr*)&address,(socklen_t*)&addrlen);
+    printf("New client connected from %s:%d\n",inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+}
 
-	return 0; 
-} 
+void printmessage(int socketfd, char message[]){
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    getpeername(socketfd,(struct sockaddr*)&address,(socklen_t*)&addrlen);
+    printf("Message from %s:%d : %s\n",inet_ntoa(address.sin_addr) , ntohs(address.sin_port),message);
+}
+
+void printdisconnect(int socketfd){
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    getpeername(socketfd,(struct sockaddr*)&address,(socklen_t*)&addrlen);
+    printf("Host from %s:%d disconnected\n",inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+}
+
+void * socketThread(void *arg){
+    int newSocket = *((int *)arg);  
+    while(1){   
+        if((valread=read(newSocket,buffer,1024))==0){
+            break;
+        }
+        else{
+            if(strlen(buffer)==0){
+                sleep(RTT);
+                continue;
+            }
+            else{
+                send(newSocket,"Hello Client\n",strlen("Hello Client\n"),0);
+                printmessage(newSocket,buffer);
+                sleep(RTT);
+            }
+        }
+    }
+    printdisconnect(newSocket);
+    close(newSocket);
+    pthread_exit(NULL);
+}
+
+int main(int argc,char *argv[]){
+    if(argc != 2){
+        printf("Missing/Extra Arguments provided.\n");
+        printf("Usage:- ./<output_file> <Server Port>\n");
+        return -1;
+    }
+    //checking for whether correct number of arguments are provided
+    int port = str_to_pnum(argv[1]);
+    if(port==-1){
+        printf("Invalid Port Number Must be in range [0-65535]\n");
+        return -1;
+    }
+    //checking for port number
+    int serverSocket, newSocket;
+    struct sockaddr_in serverAddr;
+    struct sockaddr_storage serverStorage;
+    socklen_t addr_size;
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    //Initializing the socket, The socket function creates an unbound socket and returns its file descriptor. SOCK_STREAM provides sequenced and reliable data transfer
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    // htons:- host to network short, converts the given port number to hexa decimal depending on what machine type is it, big endian or little endian
+
+    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+    // filling server_address struct to all 0's
+
+    bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    // Binding the server socket to the port passed in command line.
+    //Listen on the socket, with 40 max connection requests queued 
+
+    if(listen(serverSocket,MAX_PENDING_CONN)==0){
+        printf("Listening\n");
+    }
+    else{
+        printf("Error\n");
+    }
+
+    pthread_t tid[60];
+
+    int i = 0;
+
+    while(1){
+    //Accept call creates a new socket for the incoming connection
+        addr_size = sizeof(serverStorage);
+        newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
+        //for each client request creates a thread and assign the client request to it to process
+        //so the main thread can entertain next request
+        printnew(newSocket);
+        if(pthread_create(&tid[i],NULL,socketThread,&newSocket)!=0){
+            printf("Failed to create thread\n");
+        }
+        if(i>=MAX_PENDING_CONN){
+            i = 0;
+            while(i<MAX_PENDING_CONN){
+                pthread_join(tid[i++],NULL);
+            }
+            i = 0;
+        }
+    }
+    return 0;
+}
